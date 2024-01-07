5f80e0c4f8dd767cdecc5cd9e8de1c4244603b33
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-01-07 15:09:13 +0300

    creating files and navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* import './App.css' */
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
